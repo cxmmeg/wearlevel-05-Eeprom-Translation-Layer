6984b5c4cf221265f3d812a4a9c914072ea2d7ce
Howard0o0 <837971940@qq.com>
2020-10-14 16:34:05 +0800

    draft dual-pool

---
+++ etl.h
@@ -0,0 +1,33 @@
+#ifndef ETL_H_
+#define ETL_H_
+
+/*
+ * physical data page structure :
+ * --------------------
+ * Erase Cycle          [2B]
+ * Effective Erase Cyle [2B]
+ * Logic Page           [1B/2B]
+ * Hot                  [1B]
+ * check sum            [1B]
+ * data                 [8B?]
+ * -------------------
+ *
+ * info page :
+ * -------------------
+ * logic page size      [1B]
+ * tital page count     [2B]
+ * thresh hold          [2B]
+ * -------------------
+ *
+ * dual-pool algorithm :
+ * Dirty Swap:
+ *      MaxQue<EC,HP>.front - MaxQue<EC,CP>.back > TH
+ *
+ * Cold Pool Resize :
+ *      MaxQue<EEC,CP>.front - MaxQue<EEC,HP>.back > TH
+ *
+ * Hot Pool Resize :
+ *      MaxQue<EC,HP>.front - MaxQue<EC,HP>.back > 2*TH
+ */
+
+#endif
